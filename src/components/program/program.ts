cd7cab7a5f7366615ee7c0fff61301c58eff0088
Heavens Milien <h_milien@hotmail.com>
2018-09-11 20:46:14 -0400

    update program component

--- src/components/program/program.ts
+++ src/components/program/program.ts
@@ -12,26 +12,25 @@ import { Component, Output, Input } from '@angular/core';
 })
 export class ProgramComponent {
   
-  @Input()name :string = "";
-  @Input() description :string = "";
-  @Input() summary :string = "";
-  @Input() title :string = "";
-  @Input() statistics :string = "";
-  
-  constructor() {
+    @Input()
+    name :string = "High intensity workout";
+    @Input() 
+    description :string = "Great workout for burning fat, boosting endurance,and building explosive speed and strength.";
+    @Input() 
+    summary :string = "High intensity interval training (HIIT) is one of the best ways to get fit quick";
+    @Input() 
+    title :string = "Developped by K Michael";
+    @Input() 
+    statistics :string = "2M downloads";
     
-    this.name = "High intensity workout";
-    this.description = "Great workout for burning fat, boosting endurance,and building explosive speed and strength.";
-    this.statistics = "2M downloads";
-    this.summary = "High intensity interval training (HIIT) is one of the best ways to get fit quick";
-    this.title = "Developped by K Michael";
-  }
+    constructor() {
+    }
 
-  preview(){
+    preview(){
 
-  }
-  buy(){
-    
-  }
+    }
+    buy(){
+      
+    }
 
 }
